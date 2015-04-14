873e68f7fe99e78b8982b599efef98d5401492f8
John Bellone <jbellone@bloomberg.net>
2015-04-14 12:51:18 -0400

    Modify default attributes to use new environment.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,14 +8,12 @@ default['zookeeper-cluster']['cluster_name'] = 'zookeeper'
 default['zookeeper-cluster']['cluster_node_type'] = 'participant'
 default['zookeeper-cluster']['cluster_node_id'] = 1
 
-default['zookeeper-cluster']['jvm']['server_jvmflags'] = ''
-default['zookeeper-cluster']['jvm']['jmxport'] = 9_010
-
 default['zookeeper-cluster']['username'] = 'zookeeper'
 default['zookeeper-cluster']['groupname'] = 'zookeeper'
 
 default['zookeeper-cluster']['service_name'] = 'zookeeper'
 default['zookeeper-cluster']['service_init_type'] = :sysvinit
+default['zookeeper-cluster']['environment']['jmxport'] = 9_010
 
 default['zookeeper-cluster']['version'] = '3.4.6'
 default['zookeeper-cluster']['remote_checksum'] = '01b3938547cd620dc4c93efe07c0360411f4a66962a70500b163b59014046994'
