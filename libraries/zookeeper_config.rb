0253201fe3299ddc4ccd22a69ad06861b327d322
John Bellone <jbellone@bloomberg.net>
2015-07-30 15:12:53 -0400

    Moves resource/provider into separate modules.
    This is mainly to clean up the Chef::Provider and Chef::Resource namespaces.

--- libraries/zookeeper_config.rb
+++ libraries/zookeeper_config.rb
@@ -6,69 +6,75 @@
 #
 require 'poise'
 
-class Chef::Resource::ZookeeperConfig < Chef::Resource
-  include Poise(fused: true)
-  provides(:zookeeper_config)
+module ZookeeperClusterCookbook
+  module Resource
+    class ZookeeperConfig < Chef::Resource
+      # Resource to write out Zookeeper configurations.
+      # @since 1.0.0
+      include Poise(fused: true)
+      provides(:zookeeper_config)
 
-  attribute(:path, kind_of: String, name_attribute: true, cannot_be: :empty)
-  attribute(:user, kind_of: String, default: 'zookeeper', cannot_be: :empty)
-  attribute(:group, kind_of: String, default: 'zookeeper', cannot_be: :empty)
+      attribute(:path, kind_of: String, name_attribute: true, cannot_be: :empty)
+      attribute(:user, kind_of: String, default: 'zookeeper', cannot_be: :empty)
+      attribute(:group, kind_of: String, default: 'zookeeper', cannot_be: :empty)
 
-  attribute(:instance_name, kind_of: String, required: true)
-  attribute(:data_dir, kind_of: String, default: '/var/lib/zookeeper')
-  attribute(:client_port, kind_of: Integer, default: 2181)
-  attribute(:leader_port, kind_of: Integer, default: 2888)
-  attribute(:election_port, kind_of: Integer, default: 3888)
-  attribute(:ensemble, kind_of: Array, default: [])
-  attribute(:properties, option_collector: true, default: {})
+      attribute(:instance_name, kind_of: String, required: true)
+      attribute(:data_dir, kind_of: String, default: '/var/lib/zookeeper')
+      attribute(:client_port, kind_of: Integer, default: 2181)
+      attribute(:leader_port, kind_of: Integer, default: 2888)
+      attribute(:election_port, kind_of: Integer, default: 3888)
+      attribute(:ensemble, kind_of: Array, default: [])
+      attribute(:properties, option_collector: true, default: {})
 
-  def myid
-    ensemble.index(instance_name).next.to_s
-  end
-
-  # Outputs the +properties+ in the Java Properties file format. This is
-  # what Zookeeper daemon consumes to tweak its internal configuration.
-  def to_s
-    servers = ensemble.map { |n| "server.#{ensemble.index(n).next}:#{n}:#{leader_port}:#{election_port}" }
-    properties.merge(
-      'dataDir' => data_dir,
-      'leaderPort' => leader_port,
-      'clientPort' => client_port,
-      'electionPort' => election_port).map { |kv| kv.join('=') }.concat(servers).join("\n")
-  end
-
-  action(:create) do
-    notifying_block do
-      directory ::File.dirname(new_resource.path) do
-        recursive true
-        mode '0755'
+      def myid
+        ensemble.index(instance_name).next.to_s
       end
 
-      directory new_resource.data_dir do
-        recursive true
-        mode '0755'
+      # Outputs the +properties+ in the Java Properties file format. This is
+      # what Zookeeper daemon consumes to tweak its internal configuration.
+      def to_s
+        servers = ensemble.map { |n| "server.#{ensemble.index(n).next}:#{n}:#{leader_port}:#{election_port}" }
+        properties.merge(
+          'dataDir' => data_dir,
+          'leaderPort' => leader_port,
+          'clientPort' => client_port,
+          'electionPort' => election_port).map { |kv| kv.join('=') }.concat(servers).join("\n")
       end
 
-      file ::File.join(new_resource.data_dir, 'myid') do
-        content new_resource.myid
-        mode '0644'
+      action(:create) do
+        notifying_block do
+          directory ::File.dirname(new_resource.path) do
+            recursive true
+            mode '0755'
+          end
+
+          directory new_resource.data_dir do
+            recursive true
+            mode '0755'
+          end
+
+          file ::File.join(new_resource.data_dir, 'myid') do
+            content new_resource.myid
+            mode '0644'
+          end
+
+          file new_resource.path do
+            content new_resource.to_s
+            mode '0644'
+          end
+        end
       end
 
-      file new_resource.path do
-        content new_resource.to_s
-        mode '0644'
-      end
-    end
-  end
+      action(:delete) do
+        notifying_block do
+          directory new_resource.data_dir do
+            action :delete
+          end
 
-  action(:delete) do
-    notifying_block do
-      directory new_resource.data_dir do
-        action :delete
-      end
-
-      directory ::File.dirname(new_resource.path) do
-        action :delete
+          directory ::File.dirname(new_resource.path) do
+            action :delete
+          end
+        end
       end
     end
   end
--- libraries/zookeeper_service.rb
+++ libraries/zookeeper_service.rb
@@ -6,135 +6,141 @@
 #
 require 'poise_service/service_mixin'
 
-# Resource for managing the Zookeeper service on an instance.
-# @since 1.0.0
-class Chef::Resource::ZookeeperService < Chef::Resource
-  include Poise
-  provides(:zookeeper_service)
-  include PoiseService::ServiceMixin
-  default_action(:enable)
+module ZookeeperClusterCookbook
+  # Resource for managing the Zookeeper service on an instance.
+  # @since 1.0.0
+  module Resource
+    class ZookeeperService < Chef::Resource
+      include Poise
+      provides(:zookeeper_service)
+      include PoiseService::ServiceMixin
+      default_action(:enable)
 
-  # @!attribute service_name
-  # @return [String]
-  attribute(:service_name, kind_of: String, name_attribute: true)
+      # @!attribute service_name
+      # @return [String]
+      attribute(:service_name, kind_of: String, name_attribute: true)
 
-  # @!attribute version
-  # @return [String]
-  attribute(:version, kind_of: String, required: true)
+      # @!attribute version
+      # @return [String]
+      attribute(:version, kind_of: String, required: true)
 
-  # @!attribute install_method
-  # @return [Symbol]
-  attribute(:install_method, default: 'binary', equal_to: %w{binary package})
+      # @!attribute install_method
+      # @return [Symbol]
+      attribute(:install_method, default: 'binary', equal_to: %w{binary package})
 
-  # @!attribute install_path
-  # @return [String]
-  attribute(:install_path, kind_of: String, default: '/srv')
+      # @!attribute install_path
+      # @return [String]
+      attribute(:install_path, kind_of: String, default: '/srv')
 
-  # @!attribute user
-  # @return [String]
-  attribute(:user, kind_of: String, default: 'zookeeper')
+      # @!attribute user
+      # @return [String]
+      attribute(:user, kind_of: String, default: 'zookeeper')
 
-  # @!attribute group
-  # @return [String]
-  attribute(:group, kind_of: String, default: 'zookeeper')
+      # @!attribute group
+      # @return [String]
+      attribute(:group, kind_of: String, default: 'zookeeper')
 
-  # @!attribute environment
-  # @return [String]
-  attribute(:environment, kind_of: Hash, default: lazy { default_environment })
+      # @!attribute environment
+      # @return [String]
+      attribute(:environment, kind_of: Hash, default: lazy { default_environment })
 
-  # @!attribute package_name
-  # @return [String]
-  attribute(:package_name, kind_of: String, default: 'zookeeper')
+      # @!attribute package_name
+      # @return [String]
+      attribute(:package_name, kind_of: String, default: 'zookeeper')
 
-  # @!attribute binary_url
-  # @return [String]
-  attribute(:binary_url, kind_of: String)
+      # @!attribute binary_url
+      # @return [String]
+      attribute(:binary_url, kind_of: String)
 
-  # @!attribute binary_url
-  # @return [String]
-  attribute(:binary_checksum, kind_of: String)
+      # @!attribute binary_url
+      # @return [String]
+      attribute(:binary_checksum, kind_of: String)
 
-  # @!attribute data_dir
-  # @return [String]
-  attribute(:data_dir, kind_of: String, default: '/var/lib/zookeeper')
+      # @!attribute data_dir
+      # @return [String]
+      attribute(:data_dir, kind_of: String, default: '/var/lib/zookeeper')
 
-  # @!attribute data_log_dir
-  # @return [String]
-  attribute(:log_dir, kind_of: String, default: '/var/log/zookeeper')
+      # @!attribute data_log_dir
+      # @return [String]
+      attribute(:log_dir, kind_of: String, default: '/var/log/zookeeper')
 
-  # @!attribute config_filename
-  # @return [String]
-  attribute(:config_path, kind_of: String, default: '/etc/zookeeper/zoo.cfg')
+      # @!attribute config_filename
+      # @return [String]
+      attribute(:config_path, kind_of: String, default: '/etc/zookeeper/zoo.cfg')
 
-  def default_environment
-    { PATH: '/usr/local/bin:/usr/bin:/bin' }
-  end
-
-  def current_path
-    ::File.join(install_path, 'zookeeper', 'current', "zookeeper-#{version}")
-  end
-
-  def command
-    "bin/zkServer.sh start-foreground #{config_path}"
-  end
-end
-
-# Provider for managing the Zookeeper service on an instance.
-# @since 1.0.0
-class Chef::Provider::ZookeeperService < Chef::Provider
-  include Poise
-  provides(:zookeeper_service)
-  include PoiseService::ServiceMixin
-
-  def action_enable
-    notifying_block do
-      package new_resource.package_name do
-        version new_resource.version unless new_resource.version.nil?
-        only_if { new_resource.install_method == 'package' }
+      def default_environment
+        { PATH: '/usr/local/bin:/usr/bin:/bin' }
       end
 
-      libartifact_file "zookeeper-#{new_resource.version}" do
-        artifact_name 'zookeeper'
-        artifact_version new_resource.version
-        install_path new_resource.install_path
-        remote_url new_resource.binary_url % { version: new_resource.version }
-        remote_checksum new_resource.binary_checksum
-        only_if { new_resource.install_method == 'binary' }
+      def current_path
+        ::File.join(install_path, 'zookeeper', 'current', "zookeeper-#{version}")
       end
 
-      directory new_resource.data_dir do
-        recursive true
-        mode '0755'
-        owner new_resource.user
-        group new_resource.group
-      end
-
-      directory new_resource.log_dir do
-        recursive true
-        mode '0755'
-        owner new_resource.user
-        group new_resource.group
+      def command
+        "bin/zkServer.sh start-foreground #{config_path}"
       end
     end
-    super
   end
 
-  def action_disable
-    notifying_block do
-      directory new_resource.data_dir do
-        action :delete
+  # Provider for managing the Zookeeper service on an instance.
+  # @since 1.0.0
+  module Provider
+    class ZookeeperService < Chef::Provider
+      include Poise
+      provides(:zookeeper_service)
+      include PoiseService::ServiceMixin
+
+      def action_enable
+        notifying_block do
+          package new_resource.package_name do
+            version new_resource.version unless new_resource.version.nil?
+            only_if { new_resource.install_method == 'package' }
+          end
+
+          libartifact_file "zookeeper-#{new_resource.version}" do
+            artifact_name 'zookeeper'
+            artifact_version new_resource.version
+            install_path new_resource.install_path
+            remote_url new_resource.binary_url % { version: new_resource.version }
+            remote_checksum new_resource.binary_checksum
+            only_if { new_resource.install_method == 'binary' }
+          end
+
+          directory new_resource.data_dir do
+            recursive true
+            mode '0755'
+            owner new_resource.user
+            group new_resource.group
+          end
+
+          directory new_resource.log_dir do
+            recursive true
+            mode '0755'
+            owner new_resource.user
+            group new_resource.group
+          end
+        end
+        super
+      end
+
+      def action_disable
+        notifying_block do
+          directory new_resource.data_dir do
+            action :delete
+          end
+        end
+        super
+      end
+
+      def service_options(service)
+        service.command(new_resource.command)
+        service.directory(new_resource.current_path)
+        service.user(new_resource.user)
+        service.environment(new_resource.environment.merge(
+          'ZOO_LOG_DIR' => new_resource.log_dir,
+        ))
+        service.restart_on_update(true)
       end
     end
-    super
-  end
-
-  def service_options(service)
-    service.command(new_resource.command)
-    service.directory(new_resource.current_path)
-    service.user(new_resource.user)
-    service.environment(new_resource.environment.merge(
-      'ZOO_LOG_DIR' => new_resource.log_dir,
-    ))
-    service.restart_on_update(true)
   end
 end
