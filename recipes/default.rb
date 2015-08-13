0a1386c786e84f4716a1d307f25d090c8881e4d9
John Bellone <jbellone@bloomberg.net>
2015-08-13 15:32:54 -0400

    Removes the :create action from service resource.

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,5 +29,5 @@ zookeeper_service node['zookeeper-cluster']['service_name'] do |r|
   config_path node['zookeeper-cluster']['config']['path']
 
   node['zookeeper-cluster']['service'].each_pair { |k, v| r.send(k, v) }
-  action [:create, :enable]
+  action [:enable, :start]
 end
