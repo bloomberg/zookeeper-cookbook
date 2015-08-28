1bac7a031129688fa889375cc665d9d1798b45c6
John Bellone <jbellone@bloomberg.net>
2015-08-27 20:39:33 -0400

    Sets SELinux to disabled for all platforms.

--- test/spec/recipes/default_spec.rb
+++ test/spec/recipes/default_spec.rb
@@ -4,7 +4,7 @@ describe_recipe 'zookeeper-cluster::default' do
   cached(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
   it { expect(chef_run).to create_poise_service_user('zookeeper').with(group: 'zookeeper') }
-  it { expect(chef_run).to include_recipe('selinux::permissive') }
+  it { expect(chef_run).to include_recipe('selinux::disabled') }
   it { expect(chef_run).to include_recipe('java::default') }
   it { expect(chef_run).to create_zookeeper_config('zookeeper') }
   it { expect(chef_run).to enable_zookeeper_service('zookeeper') }
