f3fd0d3aac5380a73b474070dfb82581f53ada6b
Cloud User <cloud-user@ip-1-70-20-144.dob1.bcpc.bloomberg.com>
2016-01-11 12:30:57 -0500

    Added creation of log4j.properties via templating

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,6 +16,8 @@ default['zookeeper-cluster']['service_name'] = 'zookeeper'
 default['zookeeper-cluster']['service_user'] = 'zookeeper'
 default['zookeeper-cluster']['service_group'] = 'zookeeper'
 
+default['zookeeper-cluster']['service']['environment']['ZOOCFGDIR'] = '/etc/zookeeper'
+
 default['zookeeper-cluster']['service']['environment']['JMXPORT'] = 9010
 default['zookeeper-cluster']['service']['version'] = '3.5.0-alpha'
 default['zookeeper-cluster']['service']['binary_checksum'] = '87814f3afa9cf846db8d7e695e82e11480f7b19d79d8f146e58c4aefb4289bf4'
